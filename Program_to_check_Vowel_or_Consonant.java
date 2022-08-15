671b15d6d1d6813cccad7ef603f7d845b36de6e7
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-15 20:52:52 +0530

    import java.util.*; class main {     public static void main(String args[])     {         char ch;         Scanner sc=new Scanner(System.in);         ch=sc.nextLine().charAt(0);         switch(ch)         {         case 'A':         case'a':        case'E':         case'e':         case'I':         case'i':        case'O':        case'o':        case'U':        case'u':        System.out.println("Vowel");        break;        default:        System.out.println("Consonant");        break;         }     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        char ch;
+        Scanner sc=new Scanner(System.in);
+        ch=sc.nextLine().charAt(0);
+        switch(ch)
+        {
+        case 'A':
+        case'a':
+       case'E':
+        case'e':
+        case'I':
+        case'i':
+       case'O':
+       case'o':
+       case'U':
+       case'u':
+       System.out.println("Vowel");
+       break;
+       default:
+       System.out.println("Consonant");
+       break;
+        }
+    }
+}
