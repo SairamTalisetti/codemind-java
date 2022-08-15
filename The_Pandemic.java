097c566d080a9bccf204f337368d362968b9ebf3
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-15 21:08:06 +0530

    import java.util.*; class house {     public static void main(String arg[])     {        int N;         Scanner sc=new Scanner(System.in);         N=sc.nextInt();         if(N<3)            System.out.println(N-1);         else           System.out.println("2");
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class house
+{
+    public static void main(String arg[])
+    {
+       int N;
+        Scanner sc=new Scanner(System.in);
+        N=sc.nextInt();
+        if(N<3)
+           System.out.println(N-1);
+        else
+          System.out.println("2");
+    
+    }
+}
