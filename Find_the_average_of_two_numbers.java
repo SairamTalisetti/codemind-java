e3d657b2e708898311783059ad2e6add5f3b11dd
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-17 22:29:26 +0530

    import java.util.*; class main {     public static void main(String args[])     {       Scanner sc=new Scanner(System.in);       int a,b;       float c;       a=sc.nextInt();       b=sc.nextInt();       c=(float)(a+b)/2;       System.out.format("%.4f",c);     }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int a,b;
+      float c;
+      a=sc.nextInt();
+      b=sc.nextInt();
+      c=(float)(a+b)/2;
+      System.out.format("%.4f",c);
+    }
+   
+}
