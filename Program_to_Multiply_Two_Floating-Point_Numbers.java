648bf19f54c4d33baeaad9ec5e0efa4a6ff86c41
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-18 21:39:07 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float a,b,c;         a=sc.nextFloat();         b=sc.nextFloat();         c=(float) (a*b);         System.out.format("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float a,b,c;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        c=(float) (a*b);
+        System.out.format("%.2f",c);
+    }
+}
