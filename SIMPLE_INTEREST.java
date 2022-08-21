6124428225b0e06362e67ed1181feee88ef587d2
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-21 23:35:06 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int P,T,R;         int s;         P=sc.nextInt();         T=sc.nextInt();         R=sc.nextInt();         s=(P*T*R)/100;         System.out.println(s);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int P,T,R;
+        int s;
+        P=sc.nextInt();
+        T=sc.nextInt();
+        R=sc.nextInt();
+        s=(P*T*R)/100;
+        System.out.println(s);
+    }
+}
