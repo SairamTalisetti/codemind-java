16c598ad5662bf2e928da8e7515eff2c4df7ff02
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-23 20:17:37 +0530

    import java.util.*; class main {     public static void main(String aegs[])     {         Scanner sc=new Scanner(System.in);         int c;         float f;         c=sc.nextInt();         f=(5f/9)*(c-32);         System.out.format("%.2f",f);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main
+{
+    public static void main(String aegs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int c;
+        float f;
+        c=sc.nextInt();
+        f=(5f/9)*(c-32);
+        System.out.format("%.2f",f);
+    }
+}
