84de81ba574802e5b84e7d403d60cc6ca4bd95cb
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-08-27 14:03:19 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int t,n1,n2,sum;         t=sc.nextInt();         while(t>0)         {
                 n1=sc.nextInt();
                 n2=sc.nextInt();
                 sum=n1+n2;
                 System.out.println(sum);
                 t--;
            }
        }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t,n1,n2,sum;
+        t=sc.nextInt();
+        while(t>0)
+        {
+             
+             n1=sc.nextInt();
+             n2=sc.nextInt();
+             sum=n1+n2;
+             System.out.println(sum);
+             t--;
+        }
+    }
+}
