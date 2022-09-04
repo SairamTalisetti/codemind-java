ca2d3388a934839d9149621e67372d651550e9df
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-09-04 18:06:16 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,max;         n=sc.nextInt();         max=n%10;         while(n>0)         {             if(n%10>max)         {             max=n%10;         }         n=n/10;         }         System.out.println(max);     } }

---
+++ Largest_Digit.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,max;    
+        n=sc.nextInt();  
+        max=n%10;       
+        while(n>0)   
+        {
+            if(n%10>max)   
+        {
+            max=n%10;    
+        }
+        n=n/10;   
+        }
+        System.out.println(max);
+    }
+}
+        
+
+
+
+           
+   
