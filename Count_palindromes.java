99ba46db90cb04769d700b2d19016bb66194fd97
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-09-17 14:04:49 +0530

    import java.util.*; class main {     public static boolean ispalindrome(int x)     {         int sum=0,r,temp=x;         while(x>0)         {             r=x%10;             sum=sum*10+r;             x=x/10;         }         if(temp==sum)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],count=0;         n=sc.nextInt();         x=new int[n];         for (int i=0;i<n;i++)           x[i]=sc.nextInt();         for(int i=0;i<n;i++)         {             if(ispalindrome(x[i]))                count++;         }         System.out.println(count);     } }

---
+++ Count_palindromes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class main
+{
+    public static boolean ispalindrome(int x)
+    {
+        int sum=0,r,temp=x;
+        while(x>0)
+        {
+            r=x%10;
+            sum=sum*10+r;
+            x=x/10;
+        }
+        if(temp==sum)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for (int i=0;i<n;i++)
+          x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(ispalindrome(x[i]))
+               count++;
+        }
+        System.out.println(count);
+    }
+}
