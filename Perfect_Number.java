3d5011ba42fb605cd488c3e9da8ab261cd803ac5
SairamTalisetti <106660045+SairamTalisetti@users.noreply.github.com>
2022-10-02 22:40:17 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,r,sum=0;         n=sc.nextInt();         for(int i=1; i<n; i++)         {             if(n%i==0)             {                 sum=sum+i;             }
            }
            if(n==sum)
            System.out.println("True");
            else
            System.out.println("False");
        }
    }

---
+++ Perfect_Number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,r,sum=0;
+        n=sc.nextInt();
+        for(int i=1; i<n; i++)
+        {
+            if(n%i==0)
+            {
+                sum=sum+i;
+            }
+            
+        }
+        if(n==sum)
+        System.out.println("True");
+        else
+        System.out.println("False");
+    }
+}
