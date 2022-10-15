83eb1233c1f54f43b1fa8510bc78a462d8687ecc
T.SAIRATNAM <106660045+SairamTalisetti@users.noreply.github.com>
2022-10-15 07:20:55 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,sum=0,pro=1,i,r;         n=sc.nextInt();         while(n>0)         {             r=n%10;             sum=sum+r;             pro=pro*r;             n=n/10;         }         if(sum==pro)         System.out.println("Spy Number");         else         System.out.println("Not Spy Number");     } }

--- Spy_Number.java
+++ Spy_Number.java
@@ -1,32 +1,21 @@
 import java.util.*;
-    class Spy{
-        public static int prod(int n){
-            int pro=1,rem;
-            while(n>0){
-                rem = n%10;
-                pro= pro*rem;
-                n=n/10;
-            }
-            return pro;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,sum=0,pro=1,i,r;
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+r;
+            pro=pro*r;
+            n=n/10;
         }
-        public static int sum(int x){
-            int sum=0,r;
-            while(x>0){
-                r=x%10;
-                sum=sum+r;
-                x=x/10;
-            }
-            return sum;
-        }
-        public static void main(String []args){
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            int res,res1;
-            res = prod(n);
-            res1 = sum(n);
-            if(res==res1)
-            System.out.println("Spy Number");
-            else
-            System.out.println("Not Spy Number");
-        }
+        if(sum==pro)
+        System.out.println("Spy Number");
+        else
+        System.out.println("Not Spy Number");
     }
+}
