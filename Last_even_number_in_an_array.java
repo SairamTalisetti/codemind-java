21170b032e261a4e6e75eca2d402b4a35701981b
T.SAIRATNAM <106660045+SairamTalisetti@users.noreply.github.com>
2022-10-22 22:24:11 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],s=0;         n=sc.nextInt();         x=new int[n];         for(int i=0; i<n; i++)         {             x[i]=sc.nextInt();         }         for(int i=0; i<n; i++)         {             if(x[i]%2==0)             {                 s=x[i];             }
            }
            System.out.println(s);
        }
         
    }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],s=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(x[i]%2==0)
+            {
+                s=x[i];
+            }
+        
+        }
+        System.out.println(s);
+    }
+     
+}
+        
